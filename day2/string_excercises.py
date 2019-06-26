b8766aa8259d4e96b7131a2746258ce31a5ea286
Grzegorz Szyperek <grzegorz.szyperek@gmail.com>
2019-06-26 20:14:26 +0200

    Initial commit

---
+++ day2/string_excercises.py
@@ -0,0 +1,25 @@
+meno = "John"
+naz = "Snow"
+#CTRL + SPACE - podpowiada jakie mamy dostępne opcje
+#FN lub ALT + Shift + F10
+#by komentowacaych linii służy CTRL + /
+
+print(f'Witoj, {meno} {naz}'[-1:-6:-1])
+
+# dwa = meno + ' ' + naz
+# # print('z spacji', dwa)
+# # print('bez spacji', dwa.strip(' '))
+# #
+# # trzy = "_ucho_"
+# # print(trzy)
+# # print(trzy.strip("_"))
+# #
+# #
+# # raz = ' m a s l o    '
+# # print(raz.strip(' '))
+# # print(raz.replace(' ', ""))
+
+
+
+
+
