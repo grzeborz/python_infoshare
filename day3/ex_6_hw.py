6df1a0037869560f64a12c9b7daa35d3ecec894c
Grzegorz Szyperek <grzegorz.szyperek@gmail.com>
2019-07-03 17:43:35 +0200

    Zabawy z open, read write file.

--- day3/ex_6_hw.py
+++ day3/ex_6_hw.py
@@ -1,15 +1,21 @@
+import os
+
 reader = open('prog.py', 'w')
 filename = __name__
 filepath = __file__
+full_path = os.getcwd()
 try:
-    reader.write(f'{filename}\n {filepath}')
+    reader.write(f'{filename}\n {filepath}\n {full_path}')
 finally:
     reader.close()
 
-# with open('prog.py', 'w') as reader:
-#     print(__file__)
-#     print(__name__)
-#
-#
+imie_słuchacza = input('Podaj imie: ')
+
+with open('prog.py', 'a') as a_reader:
+    a_reader.write(f'\n {imie_słuchacza}')
+    # print(__file__)
+    # print(__name__)
+
+
 # with open('prog.py', 'w') as writer:
 #     writer.write('Yolyol')
--- day3/prog.py
+++ day3/prog.py
@@ -1,2 +1,4 @@
 __main__
- C:/Users/grzegorz.szyperek/PycharmProjects/python_infoshare/day3/ex_6_hw.py
+ ex_6_hw.py
+ C:\Users\grzegorz.szyperek\PycharmProjects\python_infoshare\day3
+ Elo Elo
