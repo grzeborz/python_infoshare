887e1e2679c30f4fa6f1f9bcf98f63ee373efee7
Grzeborz <grzegorz.szyperek@gmail.com>
2019-08-07 18:34:21 +0200

    day 12

--- day10/mail.py
+++ day10/mail.py
@@ -1,7 +1,19 @@
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+# napisac klasę o nazwie np SpamSender w oddzielnym pliku - wykorzystać kod z Day10\exercises\mail.py
+class SpamSender(object):
 
+# SpamSender w konstruktorze powinien mieć możliwość podania adresu email i hasła do skrzynki pocztowej,
+# z ktorej bedzie wysylany spam
+# nastepnie nalezy stworzyc metodę, która będzie umożliwiała wysyłąnie maila (w formacie HTML) -
+# przykład wysłania prostej wiadomości HTML w wyżej wymienionym pliku
+# do danego odbiorcy
+# najlepiej rozbić projekt na dwa pliki - jeden w ktorym bedziemy przechowywac klase, drugi w ktorym bedziemy
+# wysylac wiadomości SPAM
+# jako rozszerzenie zadania utworzyć listę emaili do których należy wysłać wiadomość SPAM,
+# i przy użyciu pętli wysłać wiadomość do wszystkich maili z listy :slightly_smiling_face:
+#jest tez w pracach domowych na repozytorium :slightly_smiling_face:
 #zadanko  pracujemy dla jednej korporacji wysłać  100 mejli do
 #podajemy spam sender
 
