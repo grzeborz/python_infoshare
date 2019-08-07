c659fe8a0627b1e61b2fb027fe4eef090ebe7735
Grzeborz <grzegorz.szyperek@gmail.com>
2019-08-07 18:52:12 +0200

    day 12 - rest api

---
+++ day12/exercises/rest_api_example.py
@@ -0,0 +1,18 @@
+import requests
+#import json
+#http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3
+
+response = requests.get('http://www.recipepuppy.com/api/', params={'i':'omions,garlic', 'q':'omlet', 'p':'1'})
+
+print(response)
+
+print(response.status_code)
+
+recipes = response.json()['results']
+
+
+for recipe in recipes:
+    print("\n")
+    print(recipe['title'])
+    print(30* '-')
+    print(recipe['ingredients'])
